3e202938ce804694b413cb15f7fa7274720d4aae
chronosgit <warfacemeister@mail.ru>
2024-06-22 23:34:24 +0500

    feat: added global styles

--- src/App.vue
+++ src/App.vue
@@ -7,3 +7,25 @@
 		name: 'App',
 	};
 </script>
+
+<style>
+	* {
+		margin: 0;
+		padding: 0;
+		font-family: 'Poppins';
+	}
+
+	img {
+		display: inline-block;
+		max-width: 100%;
+		height: auto;
+	}
+
+	a {
+		color: inherit;
+	}
+
+	button {
+		cursor: pointer;
+	}
+</style>
