530589207ec2b3b5f5c028a1a5bc9c7bd560cdf7
Nurken Kidirmaganbetov <126391668+chronosgit@users.noreply.github.com>
2024-06-25 20:33:46 +0500

    Update Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -66,7 +66,6 @@
 					this.initPosts = this.posts;
 					this.shownPosts = this.getNewShownPosts(this.curPage);
 					this.pages = getPageLabels(posts.length);
-					console.log(posts);
 					this.isLoading = false;
 					this.loaderMsg = '';
 				})
