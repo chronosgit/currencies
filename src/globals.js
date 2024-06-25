95e703f784730001ed762d951b53758fba3af4f0
chronosgit <warfacemeister@mail.ru>
2024-06-25 20:14:08 +0500

    feat: minor

--- src/globals.js
+++ src/globals.js
@@ -1,6 +1,5 @@
-const API_URL = 'https://nationalbank.kz/rss/get_rates.cfm?fdate=23.02.2009';
-// dev:
-// const API_URL = '/api/rss/get_rates.cfm?fdate=23.02.2009';
+// const API_URL = 'https://nationalbank.kz/rss/get_rates.cfm?fdate=23.02.2009';
+const API_URL = '/api/rss/get_rates.cfm?fdate=23.02.2009';
 
 export {
 	API_URL,
